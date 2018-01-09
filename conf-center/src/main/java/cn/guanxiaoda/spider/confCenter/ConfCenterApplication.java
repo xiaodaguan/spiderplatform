d111f28cd04eea92f7088e8a7cf0304250a6bf1c
guanxiaoda <guanxiaoda@meituan.com>
2018-01-09 10:21:19 +0800

    add modules

---
+++ conf-center/src/main/java/cn/guanxiaoda/spider/confCenter/ConfCenterApplication.java
@@ -0,0 +1,12 @@
+package cn.guanxiaoda.spider.confCenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfCenterApplication.class, args);
+	}
+}
---
+++ engine/src/main/java/cn/guanxiaoda/spider/engine/EngineApplication.java
@@ -0,0 +1,12 @@
+package cn.guanxiaoda.spider.engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EngineApplication.class, args);
+	}
+}
